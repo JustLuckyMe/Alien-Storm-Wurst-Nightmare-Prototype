5cf92a419447559e9d5917a38539c0b6d188de20
Alyssa <alyssacanlas20@gmail.com>
2024-02-07 18:16:39 +0200

    Added the Player Script and the Pickup script

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Pickup : MonoBehaviour
 {
-    [SerializeField] GameObject pickup;
+    public string playerTag = "Player";
+    public UnityEvent triggeredEvent;
+    public bool triggerOnce = true; // Default to true for safety
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            triggeredEvent.Invoke();
 
+            if (triggerOnce)
+            {
+                Destroy(gameObject); // "this" is not necessary when destroying the script's GameObject
+            }
+        }
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,29 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Player : MonoBehaviour
 {
-    public int Health = 100;
-    public float Sprint = 100;
-
-    public int attackDamage;
-    
-
-    public Enemy enemy;
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float currentHealth = 100f;
+    [SerializeField] private float maxEnergy = 100f;
+    [SerializeField] private float currentEnergy = 0f;
+    [SerializeField] private float baseDamage = 10f;
 
     void Awake()
     {
-        
+        // Enemy enemy = GetComponent<Enemy>();
     }
 
-    void Update()
+    // Player movement (animation will be separate just for convenience)
+    void Move() { }
+
+    void Block() { }
+
+    void Jump() { }
+
+    public void TakeDamage(float enemyDmg)
     {
-        
+        if (currentHealth > 0)
+        {
+            currentHealth -= enemyDmg;
+
+            if (currentHealth <= 0)
+            {
+                PlayerDeath();
+            }
+        }
     }
 
-    public void takeDamage()
+    public void GainHealth(float healthAmount)
     {
-        Health -= enemy.enemyAttack;
+        Debug.Log("Player picked up a health pack!");
+
+        if (currentHealth < maxHealth)
+        {
+            currentHealth += healthAmount;
+            currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+        }
+        else
+        {
+            Debug.Log("Player already has max health, health doesn't change");
+        }
     }
+
+    public void GainEnergy(float energyAmount)
+    {
+        Debug.Log("Player gained energy!");
+
+        if (currentEnergy < maxEnergy)
+        {
+            currentEnergy += energyAmount;
+            currentEnergy = Mathf.Clamp(currentEnergy, 0f, maxEnergy);
+        }
+        else
+        {
+            Debug.Log("Player already has max energy");
+        }
+    }
+
+    void PlayerDeath()
+    {
+        // Play death animation
+        // Play game over screen
+    }
+
 }
